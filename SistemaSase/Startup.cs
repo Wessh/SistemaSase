80413e5997347b5a90a10bbdfecb1194a0c42fd9
Wessh <wesshbr@gmail.com>
2020-07-10 09:11:48 -0300

    Alteracao

---
+++ SistemaSase/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SistemaSase.Startup))]
+
+namespace SistemaSase
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Para obter mais informações sobre como configurar seu aplicativo, visite https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
